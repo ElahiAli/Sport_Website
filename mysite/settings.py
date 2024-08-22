5a17db874149d2e06fa07ea9aed7503a54edf507
ElahiALi <ali_elahi00@outlook.com>
2024-08-22 18:39:40 +0330

    edit setting for vercel

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u_xsj_z9o#0-%*034l2b!4a82(25l(nafo0^7bbk)#ek0nz$d1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
